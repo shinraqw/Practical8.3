d787fbac38420a473d5e5f5893130867f980b02d
shinraqw <oliynik05vitaliy@gmail.com>
2023-06-23 12:29:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Завдання 4</title>
+</head>
+<body>
+<h1>Завдання 4 - Рядки</h1>
+<form action="result.php" method="post">
+  <label for="e">Змінна E:</label>
+  <input type="text" name="e" id="e"><br>
+  
+  <label for="f">Змінна F:</label>
+  <input type="text" name="f" id="f"><br>
+  
+  <input type="submit" value="Відправити">
+</form>
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Завдання 4 - Результат</title>
+</head>
+<body>
+<h1>Завдання 4 - Результат</h1>
+<?php
+$e = $_POST['e'];
+$f = $_POST['f'];
+
+echo "а) $e, $f <br>";
+echo "б) $f, $e <br>";
+?>
+</body>
+</html>
